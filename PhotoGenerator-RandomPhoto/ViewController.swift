9cd001792571bb216749130ee8cb24292320db62
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2021-12-20 14:44:49 +0330

    Initial Commit

---
+++ PhotoGenerator-RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoGenerator-RandomPhoto
+//
+//  Created by Omid Heydarzadeh on 12/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
